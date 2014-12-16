a5790002c21638002f35d013e3a55311547a6892
meza <meza@meza.hu>
2014-12-16 22:17:19 +0000

    initial commit

---
+++ src/TailoredTunes/MongoUserSessionDao/MongoSessionDao.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace TailoredTunes\Dao;
+
+use TailoredTunes\MongoDbConnection;
+use TailoredTunes\UserSession\Dao\SessionDao;
+
+class MongoSessionDao implements SessionDao
+{
+
+    private $maxTime = 10080;
+
+    /**
+     * @var MongoDbConnection
+     */
+    private $connection;
+
+    public function __construct(MongoDbConnection $connection)
+    {
+
+        $this->connection = $connection;
+    }
+
+    public function open()
+    {
+        return true;
+    }
+
+    public function close()
+    {
+        return true;
+    }
+
+    public function read($id)
+    {
+        $doc = $this->connection->sessions->findOne(["_id" => $id], ["sessionData" => 1]);
+        return $doc['sessionData'];
+    }
+
+    public function write($id, $data)
+    {
+        $this->connection->sessions->save(["_id" => $id, "sessionData" => $data, "timeStamp" => time()]);
+        return true;
+    }
+
+    public function destroy($id)
+    {
+        $this->connection->sessions->remove(["_id" => $id]);
+        return true;
+    }
+
+    public function gc()
+    {
+        $agedTime = time() - $this->maxTime;
+        $this->connection->sessions->remove(["timeStamp" => ['$lt' => $agedTime]]);
+    }
+}
